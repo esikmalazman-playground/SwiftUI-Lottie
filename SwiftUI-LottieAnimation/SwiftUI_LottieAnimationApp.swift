999dbb7806006bf4cac7a0bc117e65a6307ec27e
Ikmal Azman <opikmalazman@gmail.com>
2023-07-12 10:22:10 +0100

    Initial Commit

---
+++ SwiftUI-LottieAnimation/SwiftUI_LottieAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LottieAnimationApp.swift
+//  SwiftUI-LottieAnimation
+//
+//  Created by Ikmal Azman on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LottieAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
